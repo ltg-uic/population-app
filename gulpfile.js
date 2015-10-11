563c3e9eb5179fced42325114d7f465b1e35825a
Anthony Perritano <aperritano@gmail.com>
2015-10-11 15:54:25 -0500

    fixed date endpoint problem

--- gulpfile.js
+++ gulpfile.js
@@ -276,7 +276,7 @@ gulp.task('default', ['clean'], function (cb) {
   runSequence(
     ['copy', 'styles'],
     'elements',
-    ['images', 'fonts', 'html'],
+    ['jshint', 'images', 'fonts', 'html'],
     'vulcanize','rename-index', // 'cache-config',
     cb);
 });
